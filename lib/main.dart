50a162898f77b051c7e27c166730ff5adb6c9b25
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-08-26 01:24:49 +0500

    Adding outlined button.icon!

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './providers/user_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -21,6 +22,9 @@ class MyApp extends StatelessWidget {
               .copyWith(secondary: Colors.amber).copyWith(background: Colors.black),
         ),
         home: const PlaceListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (context) => const  AddPlaceScreen()
+        },
       ),
     );
   }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './add_place_screen.dart';
+
 class PlaceListScreen extends StatelessWidget {
   const PlaceListScreen({Key? key}) : super(key: key);
 
@@ -9,7 +11,9 @@ class PlaceListScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Places'),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
+          IconButton(onPressed: (){
+            Navigator.pushNamed(context, AddPlaceScreen.routeName);
+          }, icon: const Icon(Icons.add))
         ],
       ),
       body: const Center(
