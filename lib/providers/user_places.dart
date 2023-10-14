789e3afa6845e295310056fdddc7b2ace6f85e36
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-10-14 21:03:50 +0500

    fixing some errors! Now its working fine!

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class UserPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -9,9 +10,15 @@ class UserPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlaces (String title, File imageFile){
-    final newPlace = Place(id: DateTime.now().toString(), title: title, location: PlaceLocation(latitude: 1, longitude: 1), image: imageFile);
+  void addPlaces(String title, File imageFile) {
+    final newPlace = Place(
+        id: DateTime.now().toString(),
+        title: title,
+        location: PlaceLocation(latitude: 1, longitude: 1),
+        image: imageFile);
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('places',
+        {'id': newPlace.id, 'title': newPlace.title, 'image': newPlace.image});
   }
 }
