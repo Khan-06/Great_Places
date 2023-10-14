27cf771a16cc1ab1cbffdfe882299b95af13da8f
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-10-14 20:31:45 +0500

    fixing some errors! Now its working fine!

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,10 +1,10 @@
-import 'package:sqflite/sqflite.dart'as sql;
+import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
 import 'package:flutter/material.dart';
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data)async {
-    final dbpath = await sql.getDatabasesPath();
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final dbPath = await sql.getDatabasesPath();
+    sql.openDatabase(path.join(dbPath, 'places.db'));
   }
 }
-
