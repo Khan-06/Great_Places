865693db3abf9875f3fae5a88cccff1a8500bf10
Khan-06 <m.abdulbasit.k.06@gmail.com>
2023-10-16 20:15:57 +0500

    fixing some errors! Now its working fine!

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -2,13 +2,22 @@ import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
 import 'package:flutter/material.dart';
 import 'package:sqflite/sql.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data) async {
+
+  static Future<Database> dataBase () async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(path.join(dbPath, 'places.db'), onCreate: (db,version){
+    return await sql.openDatabase(path.join(dbPath, 'places.db'), onCreate: (db,version){
       return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
     }, version: 1);
-    await sqlDb.insert(table, data, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.dataBase();
+        db.insert(table, data, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+  static Future<void> getData (String table) async {
+    final db = await DBHelper.dataBase();
+    db.query(table);
   }
 }
